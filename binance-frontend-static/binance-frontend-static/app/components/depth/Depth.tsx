5138cc586eb636a75317d879a8dcf916600e3267
atharv-asr-group <atharv21240@iiitd.ac.in>
2025-09-30 08:01:41 +0530

    depth component added

---
+++ binance-frontend-static/binance-frontend-static/app/components/depth/Depth.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getDepth, getTicker, getTrades } from "../../utils/httpClient";
+import { BidTable } from "./BidTable";
+import { AskTable } from "./AskTable";
+
+export function Depth({ market }: {market: string}) {
+    const [bids, setBids] = useState<[string, string][]>();
+    const [asks, setAsks] = useState<[string, string][]>();
+    const [price, setPrice] = useState<string>();
+
+    useEffect(() => {
+        getDepth(market).then(d => {
+            setBids(d.bids.reverse());
+            setAsks(d.asks);
+        });
+
+        getTicker(market).then(t => setPrice(t.lastPrice));
+    }, [])
+    
+    return <div>
+        <TableHeader />
+        {asks && <AskTable asks={asks} />}
+        {price && <div>{price}</div>}
+        {bids && <BidTable bids={bids} />}
+    </div>
+}
+
+
+function TableHeader(){
+    return <div className="flex justify-between text-xs">
+        <div className="text-white">Price</div>
+        <div className="text-slate-500">Size</div>
+        <div className="text-slate-500">Total</div>
+    </div>
+}
